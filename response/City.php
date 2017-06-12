c99f69c42baaabec3b7407733dd276cd7becc2e7
Dmitriy Samborskiy <work@samborskiy.com>
2017-06-13 01:59:05 +0300

    Данные по городам. мелкие улучшения.

--- response/City.php
+++ response/City.php
@@ -9,6 +9,13 @@ namespace stp\spsr\response;
  * @property int $City_owner_ID идентификаторы города
  * @property string $CityName Электрогорск
  * @property string $RegionName Московская обл.
+ * @property int $Region_ID идентификаторы региона
+ * @property int $Region_Owner_ID идентификаторы региона
+ * @property int $Country_ID идентификаторы города
+ * @property int $Country_Owner_ID идентификаторы города
+ * @property int $COD 1/0 Возможность использования в городе услуги «Оплата товара в момент вручения»
+ * @property int $DepId Служебная информация
+ * @property int $DepOwnerId Служебная информация
  */
 class City extends BaseResponse
 {
